045d96142efc8ae0d5de10414a1cbc32650c8b7f
lexvonghost <lex_von_tost@mail.ru>
2022-07-28 00:10:17 +0300

    feat: решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    double cube = Math.Pow(i, 3);
+    Console.WriteLine(cube);
+}
+//  Решение №2
+int j = 1;
+while (j <= n)
+{
+    double cube = Math.Pow(j, 3);
+    Console.WriteLine(cube);
+    j++;
+}
