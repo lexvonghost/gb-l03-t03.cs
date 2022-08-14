4a4af4c668db534fc99eca11469979962e2d3941
lexvonghost <lex_von_tost@mail.ru>
2022-08-14 14:28:51 +0300

    build: добавил условия

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число");
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число");
 int n = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 1; i <= n; i++)
